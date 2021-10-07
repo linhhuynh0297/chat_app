c3b8f6ff18c89ade2ae6f85934e20f490c333faf
Loki <linhhuynh0297@gmail.com>
2021-10-07 15:48:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = 'faac900d-82d5-4c18-9705-5105cf5e83ad';
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
